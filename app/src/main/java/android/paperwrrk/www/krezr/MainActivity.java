9b65eb7d193eb34cae58c69d445e552a0a4cf687
kkdroidgit <kartikdroid17@gmail.com>
2017-09-07 08:25:07 +0530

    NavDrawer Added

--- app/src/main/java/android/paperwrrk/www/krezr/MainActivity.java
+++ app/src/main/java/android/paperwrrk/www/krezr/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        new DrawerBuilder().withActivity(this).build();
+        new DrawerBuilder().withActivity(this).build();   
     }
 }
